bfa1ee2f18ccc7285a15bab2007b408dfb039596
smartinez <sergixx31@gmail.com>
2024-04-08 12:25:49 +0200

    cambio de archivos

--- main.py
+++
@@ -1,9 +0,0 @@
-# DOCUMENTACION C++ BY SERGI M
-
-# EDITAR ARCHIVO CON NVIM -> nvim nombre_archivo
-# "i" P:ara poder escribir
-# "v" Para seleccionar texto
-# ":q" Para salir del archivo cuando no haya cambios pendientes sin guardar
-# ":q!" Sale del archivo descartando cambios sin guardar
-# ":w" Guarda los cambios pero sigues editando
-# ":wq" Guarda los cambios y sale de vim
