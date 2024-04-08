45f7c5b200d3e54924fecd8e41a36d8358cbc629
smartinez <sergixx31@gmail.com>
2024-04-08 14:42:13 +0200

    add new variables

--- Main.cpp
+++ Main.cpp
@@ -9,7 +9,34 @@ using namespace std;
 int main(){
 	
 	/*indica que va a imprimir texto*/
-	cout<<"First hello world ";
-	
+	/*cout<<"First hello world ";
+	cout<<"\n Este es otro texto \n";*/
+
+	//int edad = 20;
+	//cout<<edad;
+	//cout<<"\n";
+ 	
+	int entero = 44;
+	cout<<entero;
+	cout<<"\n";
+
+	float flotante = 4.2233;
+	cout<<flotante;
+	cout<<"\n";
+
+	double doble = 3.1416;
+	cout<<doble;
+	cout<<"\n";
+
+	//se indica con comilla simple pk si lo hacemos con doble le indicamos cadena de texto
+	char letra = 'A';
+	cout<<letra;
+	cout<<"\n";
+
+	bool booleano = true;
+	cout<<booleano;
+	cout<<"\n";
+
+
 	return 0;	
 }
