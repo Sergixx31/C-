952896b588c9fcd9f29a1e8b77ad2a9c74815670
smartinez <sergixx31@gmail.com>
2024-04-08 13:06:49 +0200

    add new hello world

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+/*iostream -> libreria de entrada y salida de datos */
+
+/*nombre de espacio*/
+using namespace std;
+
+/*funcion main*/
+int main(){
+	
+	/*indica que va a imprimir texto*/
+	cout<<"First hello world ";
+	
+	return 0;	
+}
