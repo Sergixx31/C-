36d160127afa7713e61b2bb5edbf729f9c8aa3bd
smartinez <sergixx31@gmail.com>
2024-04-15 15:16:14 +0200

    añadida calculadora interes compuesto funcional

--- Programas/CalculadoraInteresCompuesto.cpp
+++ Programas/CalculadoraInteresCompuesto.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <clocale>
+#include <cmath>
 
 using namespace std;
 
 int main(){
 	//Colocar caracteres especiales en español, accentos y ñ
-	setlocate(LC_CTYPE, "Spanish");
+	setlocale(LC_CTYPE, "Spanish");
 
 	//CapitalInicial
 	float Ci = 0;
@@ -12,25 +14,25 @@ int main(){
 	float Ti = 0;
 	//Tiempo
 	int t = 0;
-
+	float resultado = 0;
 
 	while(Ci <= 0){
 		cout<<"Ingrese el Capital Inicial: ";
-		cin<<Ci;
+		cin>>Ci;
 	}
-	while(ti <= 0){
+	while(Ti <= 0){
 		cout<<"Ingrese la Tasa de Interes anual: ";
-		cin<<Ti;
+		cin>>Ti;
 	}
 	while(t <= 0){
 		cout<<"Ingresa el tiempo en años: ";
 		cin>>t;
 	}
 
-	
-
+	resultado = Ci * pow((1 + (Ti/100)),t);
+	cout<<"El dinero que se generó al final del tiempo de la inversion es: $"<<resultado;
 
 	//El programa, al terminar el programa espera que tecleemos cualquier tecla para acabar
-	getch();	
+	//getch();
 	return 0;
 }
